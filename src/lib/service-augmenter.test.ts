2d9e75b6faf515dd806a2f773c9f8749b839b1dd
sean-nicholas <n.dieterle@netvise.de>
2019-09-25 09:24:56 +0200

    extract mockService

--- src/lib/service-augmenter.test.ts
+++ src/lib/service-augmenter.test.ts
@@ -1,16 +1,6 @@
 import { ServiceAugmenter } from './service-augmenter'
 import { defaultOptions } from '../protect-services'
-
-class MockService {
-  private internal = 'test'
-  public async find(...args: any[]) { return `${this.internal}-find` }
-  public async get(...args: any[]) { return `${this.internal}-get` }
-  public async create(...args: any[]) { return `${this.internal}-create` }
-  public async update(...args: any[]) { return `${this.internal}-update` }
-  public async patch(...args: any[]) { return `${this.internal}-patch` }
-  public async remove(...args: any[]) { return `${this.internal}-remove` }
-  public async custom(...args: any[]) { return `${this.internal}-custom` }
-}
+import { MockService } from '../../tests/mock-service'
 
 describe('ServiceAugmenter', () => {
   it('calls checkAllowed on all standard service methods', () => {
---
+++ tests/mock-service.ts
@@ -0,0 +1,11 @@
+export class MockService {
+  private internal = 'test'
+  public async find(...args: any[]) { return `${this.internal}-find` }
+  public async get(...args: any[]) { return `${this.internal}-get` }
+  public async create(...args: any[]) { return `${this.internal}-create` }
+  public async update(...args: any[]) { return `${this.internal}-update` }
+  public async patch(...args: any[]) { return `${this.internal}-patch` }
+  public async remove(...args: any[]) { return `${this.internal}-remove` }
+  public async custom(...args: any[]) { return `${this.internal}-custom` }
+  public setup(app: any, path: string) { }
+}
