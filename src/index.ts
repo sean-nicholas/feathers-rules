e97b129357de150feb7a3e56ec453baa51e6d855
Sean Nicholas Dieterle <nicholas.dieterle@gmail.com>
2019-09-26 20:22:27 +0200

    add exports

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { protectServices, ProtectServicesOptions } from './protect-services'
+export { allow } from './hooks/allow'
+export { checkAllowed } from './hooks/check-allowed'
+export { allowedChecker } from './lib/allowed-checker'
